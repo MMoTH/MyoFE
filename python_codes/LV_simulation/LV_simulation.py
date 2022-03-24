12067ca6fe2600193f438915c7ca16d19741bf91
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2022-03-24 09:10:42 -0400

    No printing coordinates

--- python_codes/LV_simulation/LV_simulation.py
+++ python_codes/LV_simulation/LV_simulation.py
@@ -76,14 +76,6 @@ class LV_simulation():
         self.local_n_of_int_points = \
             4 * np.shape(self.mesh.model['mesh'].cells())[0]
         
-        print 'MESH TESTSSS'
-        print self.mesh.model['mesh'].geometry()
-        d = self.local_n_of_int_points#self.mesh.model['function_spaces']['quadrature_space'].dim()
-        print d
-        n = self.mesh.model['mesh'].geometry().dim()
-        print n
-        coor = self.mesh.model['function_spaces']['quadrature_space'].tabulate_dof_coordinates().reshape(d,n)
-        print coor
         # Calculate the total no of integration points
         # First on the root core
         self.global_n_of_int_points = \
@@ -407,7 +399,10 @@ class LV_simulation():
                     for j in range(self.local_n_of_int_points):
                         self.hs_objs_list[j].myof.data[p.data['variable']] +=\
                             p.data['increment']
-                
+                elif p.data['level'] == 'membranes':
+                    for j in range(self.local_n_of_int_points):
+                        self.hs_objs_list[j].memb.data[p.data['variable']] +=\
+                            p.data['increment']
         # Proceed time
         (activation, new_beat) = \
             self.hr.implement_time_step(time_step)
