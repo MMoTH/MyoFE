67f983abb77a96289a38f7a87bc28f8f402e4e09
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2022-05-12 12:44:43 -0400

    solver parameters

--- python_codes/LV_simulation/dependencies/nsolver.py
+++ python_codes/LV_simulation/dependencies/nsolver.py
@@ -131,7 +131,8 @@ class NSolver(object):
                         print "nan found in A assembly"
                     if np.isnan(b.array().astype(float)).any():
                         print 'nan found in b (Ftotal) assembly'"""
-                    solve(A, dww.vector(), b)
+                    solve(A, dww.vector(), b,solver_parameters={"linear_solver": "cg",
+                            "preconditioner": "ilu"})
                     w.vector().axpy(1.0, dww.vector())
 
                 # DEBUGGING PURPOSES #############################
