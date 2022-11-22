cf192762b806407de263e0b9cd1e08e1c14ae6b8
Hossein <42911295+HosseinSharifii@users.noreply.github.com>
2022-11-21 20:48:38 -0500

    Infarct perturbation

--- python_codes/protocol/protocol.py
+++ python_codes/protocol/protocol.py
@@ -30,6 +30,12 @@ class protocol():
             for i, b in enumerate(baro_struct['activations']):
                 self.baro_activations.append(baro_activation(
                     b, self.data['time_step']))
+        
+        self.infarct_activation = []
+        if ('infarct' in prot):
+            infarct_structure = prot['infarct']
+            for i, inf in enumerate(infarct_structure['activations']):
+                self.infarct_activation.append(infarct_activation(inf,self.data['time_step']))
 
 
 class perturbation():
@@ -59,3 +65,22 @@ class baro_activation():
         self.data['t_stop_s'] = baro_struct['t_stop_s'][0]
         self.data['t_start_ind'] = int(self.data['t_start_s'] / time_step)
         self.data['t_stop_ind'] = int(self.data['t_stop_s'] / time_step)
+        
+class infarct_activation():
+    """ Class for MI-activation """
+    def __init__(self, infarct_structure, time_step):
+        self.data = dict()
+        #self.data['level'] = infarct_structure['level'][0]
+        #self.data['variable'] = infarct_structure['variable'][0]
+        self.data['t_start_s'] = infarct_structure['t_start_s'][0]
+        self.data['t_stop_s'] = infarct_structure['t_stop_s'][0]
+        self.data['t_start_ind'] = int(self.data['t_start_s'] / time_step)
+        self.data['t_stop_ind'] = int(self.data['t_stop_s'] / time_step) 
+        self.data['t_stop_s'] = infarct_structure['t_stop_s'][0]
+        self.data['t_stop_ind'] = int(self.data['t_stop_s'] / time_step)
+        self.data['infarct_total_change'] = infarct_structure['infarct_total_change'][0]
+        n_steps = (self.data['t_stop_s'] - self.data['t_start_s']) / time_step
+        self.data['infarct_increment'] = self.data['infarct_total_change'] / n_steps
+
+        self.data['boundary_zone_total_change'] = infarct_structure['boundary_zone_total_change'][0]
+        self.data['boundary_zone_increment'] = self.data['boundary_zone_total_change'] / n_steps
