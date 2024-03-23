b7ff23c3b389aa4c1bcee7588c8995615819307f
MMMehr <m.mehri@uky.edu>
2024-03-22 20:08:28 -0400

    hyper0.3

--- demos/HCM_paper/jobs_hyper0.3/job_hyper_kappa4_k1_1.8.sh
+++ demos/HCM_paper/jobs_hyper0.3/job_hyper_kappa4_k1_1.8.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=10-00:00:00             # Time limit for the job (REQUIRED).
-#SBATCH --job-name=H20_K1.5
+#SBATCH --job-name=H4_K1.8
 #SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
 #SBATCH --mem=140G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
-#SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
+#SBATCH --partition=CAC48M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/HCM_paper/hyper0.3/logs/output.%J.out # STDOUT
 cd ../../../python_codes
-singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MYoFE/demos/HCM_paper/simulations_hyper0.3/hyper_kappa20_k1_1.5/sim_inputs/input_parameters.json
+singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MYoFE/demos/HCM_paper/simulations_hyper0.3/hyper_kappa4_k1_1.8/sim_inputs/input_parameters.json
 scontrol show job $SLURM_JOB_ID
