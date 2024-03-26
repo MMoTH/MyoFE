4c78f3f32635c129ec46fde25ec5420d84dcfd5c
MMMehr <m.mehri@uky.edu>
2024-03-26 00:16:29 -0400

    last

--- demos/HCM_paper/jobs_baseline4_8core/job_baseline4_8core_Kappa_4_k1_3.8.sh
+++ demos/HCM_paper/jobs_baseline4_8core/job_baseline4_8core_Kappa_4_k1_3.8.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=140G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
 #SBATCH --partition=SKY32M192_L# Partition/queue to run the job in. (REQUIRED)
-#SBATCH --output=/mnt/gpfs2_4m/scKappatch/mme250/HCM_paper/baseline4_8core/logs/output.%J.out # STDOUT
+#SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/HCM_paper/baseline4_8core/logs/output.%J.out # STDOUT
 cd ../../../python_codes
 singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/HCM_paper/simulations_baseline4_8core/baseline4_8core_Kappa_4_k1_3.8/sim_inputs/input_parameters.json
 scontrol show job $SLURM_JOB_ID
