71b03effa6564ccbdc7c43449107f942a2ae3004
MMMehr <m.mehri@uky.edu>
2024-03-29 13:10:37 -0400

    last_ram180

--- demos/HCM_paper/jobs_hyper0.3_32core2/job_hyper_kappa20_k1_1.5.sh
+++ demos/HCM_paper/jobs_hyper0.3_32core2/job_hyper_kappa20_k1_1.5.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=32H20_K1.5
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
-#SBATCH --mem=185G                  # memory required per node - amount of memory (in bytes)
+#SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
 #SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/HCM_paper/hyper0.3_32core2/logs/output.%J.out # STDOUT
