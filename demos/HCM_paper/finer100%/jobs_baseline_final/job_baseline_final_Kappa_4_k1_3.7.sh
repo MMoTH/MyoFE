7a90d8de3d5fa05dfa62b1b652a95eb1fc6e7c17
MMMehr <m.mehri@uky.edu>
2024-08-02 17:39:03 -0400

    last_2core_300ram

--- demos/HCM_paper/finer100%/jobs_baseline_final/job_baseline_final_Kappa_4_k1_3.7.sh
+++ demos/HCM_paper/finer100%/jobs_baseline_final/job_baseline_final_Kappa_4_k1_3.7.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=13-00:00:00             # Time limit for the job (REQUIRED).
 #SBATCH --job-name=C4_K3.7
 #SBATCH --ntasks=8
-#SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
-#SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
+#SBATCH --cpus-per-task=2           # number of CPUs (or cores) per task (same as -c).
+#SBATCH --mem=300G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
 #SBATCH --partition=SKY32M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/HCM_paper/baseline_final/logs/output.%J.out # STDOUT
