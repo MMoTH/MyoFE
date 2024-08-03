b60fae7648abe25fb4cb21099652aec0e6fd84ce
MMMehr <m.mehri@uky.edu>
2024-08-03 16:22:51 -0400

    last_1node_2nodefailed

--- demos/HCM_paper/finer100%/jobs_fibrous0.3_final/job_fibrous_kappa4_C_param_13.6.sh
+++ demos/HCM_paper/finer100%/jobs_fibrous0.3_final/job_fibrous_kappa4_C_param_13.6.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --time=13-00:00:00             # Time limit for the job (REQUIRED).
 #SBATCH --job-name=F4_K13.6
-#SBATCH --nodes=2
 #SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
-#SBATCH --mem=150G                  # memory required per node - amount of memory (in bytes)
+#SBATCH --mem=185G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
 #SBATCH --partition=SKY32M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/HCM_paper/fibrous0.3_final/logs/output.%J.out # STDOUT
