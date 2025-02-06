6bccf9e196ed96d784001e3614e23915fa176616
MMMehr <m.mehri@uky.edu>
2025-02-06 17:43:56 -0500

    growht_no_perturbation test different start times

---
+++ demos/growth_FR/MR_no_pertutb/job_MR_t50.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=13-00:00:00             # Time limit for the job (REQUIRED).
+#SBATCH --job-name=MR_t50
+#SBATCH --ntasks=32
+#SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
+#SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
+#SBATCH --account=col_jfwe223_uksr
+#SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
+#SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/gr_paper/no_perturb_MR/output.%J.out # STDOUT
+cd ../../../python_codes
+singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/MR_no_pertutb/input_parameters_t_50.json
+scontrol show job $SLURM_JOB_ID
