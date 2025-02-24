b57f958f7d1b6472707403679881055bbeb1b0eb
MMMehr <m.mehri@uky.edu>
2025-02-24 10:45:53 -0500

    HCM_fibrous baseline_test

--- demos/HCM_paper/jobs_fibrous0.3_final/job_fibrous_kappa4_C_param_13.6.sh
+++ demos/HCM_paper/jobs_fibrous0.3_final/job_fibrous_kappa4_C_param_13.6.sh
@@ -8,5 +8,5 @@
 #SBATCH --partition=SKY32M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/HCM_paper/fibrous0.3_final/logs/output.%J.out # STDOUT
 cd ../../../python_codes
-singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE_strain/MyoFE/demos/HCM_paper/simulations_fibrous0.3_final/fibrous_kappa4_C_param_13.6/sim_inputs/input_parameters.json
+singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/HCM_paper/simulations_fibrous0.3_final/fibrous_kappa4_C_param_13.6/sim_inputs/input_parameters.json
 scontrol show job $SLURM_JOB_ID
