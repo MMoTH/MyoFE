a6d10e4abd4bb755b983196cce09503dc0b67f65
MMMehr <m.mehri@uky.edu>
2025-02-25 11:57:38 -0500

    AS cases for GR

--- demos/growth_FR/AS/job_AS_Kappa_8.sh
+++ demos/growth_FR/AS/job_AS_Kappa_8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=13-00:00:00             # Time limit for the job (REQUIRED).
-#SBATCH --job-name=AS_k3
+#SBATCH --job-name=AS_k8
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
 #SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
@@ -8,5 +8,5 @@
 #SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/gr_paper/AS/output.%J.out # STDOUT
 cd ../../../python_codes
-singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/AS/input_parameters_AS_kappa3.json
+singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/AS/input_parameters_AS_kappa8.json
 scontrol show job $SLURM_JOB_ID
