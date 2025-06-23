335e68eefe94aa85541a76f1505d4af4063a3c13
MMMehr <m.mehri@uky.edu>
2025-06-23 16:04:07 -0400

    last

--- demos/growth_FR/Final_models/Baseline/job_MR_Kappa_4_baseline.sh
+++ demos/growth_FR/Final_models/Baseline/job_MR_Kappa_4_baseline.sh
@@ -7,6 +7,6 @@
 #SBATCH --account=col_jfwe223_uksr
 #SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/gr_paper/final_models/baseline/output.%J.out # STDOUT
-cd ../../../python_codes
+cd ../../../../python_codes
 singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/Final_models/Baseline/input_parameters_kappa_4_baseline.json
 scontrol show job $SLURM_JOB_ID
