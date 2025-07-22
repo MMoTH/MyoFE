1816ef1380231c0e184e949429a37a3ed7e93cad
MMMehr <m.mehri@uky.edu>
2025-07-22 19:47:07 -0400

    fine_stiffness_cases

---
+++ demos/growth_FR/Final_models/MR_growth_baro_FR_stiff/job_MR_Kappa_4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=13-00:00:00             # Time limit for the job (REQUIRED).
+#SBATCH --job-name=gr_stiff
+#SBATCH --ntasks=32
+#SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
+#SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
+#SBATCH --account=col_jfwe223_uksr
+#SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
+#SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/gr_paper/final_models/MR_growth_baro_FR_stiff/output.%J.out # STDOUT
+cd ../../../../python_codes
+singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/Final_models/MR_growth_baro_FR_stiff/input_parameters_bf_12.5.json
+scontrol show job $SLURM_JOB_ID
