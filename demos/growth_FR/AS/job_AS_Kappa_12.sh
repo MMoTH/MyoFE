ece252a87e53927f46aa221516bb856dee4c8b2c
MMMehr <m.mehri@uky.edu>
2025-08-18 14:32:26 -0400

    last

--- demos/growth_FR/AS/job_AS_Kappa_12.sh
+++ demos/growth_FR/AS/job_AS_Kappa_12.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
 #SBATCH --partition=CAC48M192_L# Partition/queue to run the job in. (REQUIRED)
-#SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/gr_paper/AS/output.%J.out # STDOUT
+#SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/AS_paper/output.%J.out # STDOUT
 cd ../../../python_codes
 singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/AS/input_parameters_AS_kappa12.json
 scontrol show job $SLURM_JOB_ID
