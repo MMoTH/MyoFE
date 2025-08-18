d205153858e50c61bce0c72114501f2f267b5d7f
MMMehr <m.mehri@uky.edu>
2025-08-18 14:14:09 -0400

    AS_models

--- demos/growth_FR/AS/job_AS_Kappa_12.sh
+++ demos/growth_FR/AS/job_AS_Kappa_12.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
 #SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
 #SBATCH --account=col_jfwe223_uksr
-#SBATCH --partition=CAL48M192_L# Partition/queue to run the job in. (REQUIRED)
+#SBATCH --partition=CAC48M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/gr_paper/AS/output.%J.out # STDOUT
 cd ../../../python_codes
 singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/AS/input_parameters_AS_kappa12.json
