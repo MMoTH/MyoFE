af31720de87cecae3d521e879b8e347b0d472b4f
MMMehr <m.mehri@uky.edu>
2025-09-04 16:27:08 -0400

    cvein_basline

--- demos/growth_FR/AS/job_baseline_kappa4_c0.035.sh
+++ demos/growth_FR/AS/job_baseline_kappa4_c0.035.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=13-00:00:00             # Time limit for the job (REQUIRED).
-#SBATCH --job-name=b_kp4
+#SBATCH --job-name=b_c0035
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c).
 #SBATCH --mem=180G                  # memory required per node - amount of memory (in bytes)
@@ -8,5 +8,5 @@
 #SBATCH --partition=SKY32M192_L# Partition/queue to run the job in. (REQUIRED)
 #SBATCH --output=/mnt/gpfs2_4m/scratch/mme250/AS_paper/output.%J.out # STDOUT
 cd ../../../python_codes
-singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/AS/input_parameters_baseline_kappa4_cvein0.05.json
+singularity exec --cleanenv /home/mme250/fenics.img  mpiexec -np $SLURM_NTASKS  python MyoFE.py LV_sim /home/mme250/MyoFE/demos/growth_FR/AS/input_parameters_baseline_kappa4_cvein0.035.json
 scontrol show job $SLURM_JOB_ID
